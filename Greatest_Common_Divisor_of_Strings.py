266861642e8a33068567815ae52614ec5cfcaa56
Niyathi <nmg2881@gmail.com>
2024-03-15 22:22:06 +0530

    Can_Place_Flowers

--- Greatest_Common_Divisor_of_Strings.py
+++ Greatest_Common_Divisor_of_Strings.py
@@ -16,9 +16,14 @@
 # Input: str1 = "LEET", str2 = "CODE"
 # Output: ""
 
-class Greatest_Common_Divisor_of_Strings:
+class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
         if str1+str2 != str2+str1:
             return ""
         from math import gcd
         return str1[:gcd(len(str1),len(str2))]
+
+solution = Solution()
+str1 = "ABCABC"
+str2 = "ABC"                
+print(solution.gcdOfStrings(str1,str2))
