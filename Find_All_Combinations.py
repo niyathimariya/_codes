2d4bc8092aa57448f7ea172d99705115dad658c4
Niyathi Mariya George <nmg2881@gmail.com>
2024-05-30 21:27:37 +0530

    Find_all_combination_in_a_array

--- Find_All_Combinations.py
+++ Find_All_Combinations.py
@@ -1,2 +1,24 @@
 #TCS NQT
 #[1,2,9]=[1,12,19,129,2,29]
+from itertools import combinations
+
+def generate_combinations(arr):
+   
+    combinations_set = set()
+    for num in arr:
+        combinations_set.add(str(num))
+
+    for i in range(2, len(arr) + 1):
+        for combo in combinations(arr, i):
+            perms = set(permutations(combo))
+            for perm in perms:
+                number = ''.join(map(str, perm))
+                sorted_number = ''.join(sorted(number))
+                combinations_set.add(sorted_number)
+    
+    return list(combinations_set)
+
+arr = [1, 2, 9]
+combinations_list = generate_combinations(arr)
+combinations_list = sorted(combinations_list, key=int)
+print(combinations_list)
