5f96f590cb243b0b7d162c4ba89f1b6100e0db62
Niyathi Mariya George <nmg2881@gmail.com>
2024-05-30 21:31:30 +0530

    Find_all_combination_in_a_array

--- Find_All_Combinations.py
+++ Find_All_Combinations.py
@@ -2,23 +2,32 @@
 #[1,2,9]=[1,12,19,129,2,29]
 from itertools import combinations
 
+from itertools import combinations, permutations
+
 def generate_combinations(arr):
-   
+    # Resultant set to store unique combinations
     combinations_set = set()
+    
+    # Include individual elements
     for num in arr:
         combinations_set.add(str(num))
-
+    
+    # Generate combinations for all lengths
     for i in range(2, len(arr) + 1):
         for combo in combinations(arr, i):
+            # Generate all permutations of the current combination
             perms = set(permutations(combo))
             for perm in perms:
+                # Join tuple elements to form numbers and sort digits
                 number = ''.join(map(str, perm))
                 sorted_number = ''.join(sorted(number))
                 combinations_set.add(sorted_number)
     
     return list(combinations_set)
 
+# Example usage
 arr = [1, 2, 9]
 combinations_list = generate_combinations(arr)
 combinations_list = sorted(combinations_list, key=int)
 print(combinations_list)
+
