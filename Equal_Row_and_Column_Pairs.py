2ef2d95b7e9bf89dc80dfe6c1138c21f5593ee28
Niyathi Mariya George <nmg2881@gmail.com>
2024-06-08 17:35:46 +0530

    Added Equal_Row_and_Column_Pairs.py

---
+++ Equal_Row_and_Column_Pairs.py
@@ -0,0 +1,32 @@
+# 2352. Equal Row and Column Pairs
+
+# Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+# A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+
+# Example 1:
+# Input: grid = [[3,2,1],[1,7,6],[2,7,7]]
+# Output: 1
+# Explanation: There is 1 equal row and column pair:
+# - (Row 2, Column 1): [2,7,7]
+
+# Example 2:
+# Input: grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]
+# Output: 3
+# Explanation: There are 3 equal row and column pairs:
+# - (Row 0, Column 0): [3,1,2,2]
+# - (Row 2, Column 2): [2,4,2,2]
+# - (Row 3, Column 2): [2,4,2,2]
+
+class Solution:
+    def equalPairs(self, grid: list[list[int]]) -> int:
+        n = len(grid)
+        count = 0
+
+        columns = [[grid[row][col] for row in range(n)] for col in range(n)]
+        
+        for row in grid:
+            for col in columns:
+                if row == col:
+                    count += 1
+        
+        return count
