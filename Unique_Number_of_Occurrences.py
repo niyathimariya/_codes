c0856a07b15271aeb617e353c566baa76c53e91c
Niyathi Mariya George <nmg2881@gmail.com>
2024-06-08 16:52:39 +0530

    Added Unique_Number_of_Occurrences.py

---
+++ Unique_Number_of_Occurrences.py
@@ -0,0 +1,28 @@
+# 1207. Unique Number of Occurrences
+
+# Given an array of integers arr, return true if the number of occurrences of each value in the array is unique or false otherwise.
+
+# Example 1:
+# Input: arr = [1,2,2,1,1,3]
+# Output: true
+# Explanation: The value 1 has 3 occurrences, 2 has 2 and 3 has 1. No two values have the same number of occurrences.
+
+# Example 2:
+# Input: arr = [1,2]
+# Output: false
+
+# Example 3:
+# Input: arr = [-3,0,1,-3,1,1,1,-3,10,0]
+# Output: true
+
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        count = {}
+        for num in arr:
+            if num in count:
+                count[num] += 1
+            else:
+                count[num] = 1
+        
+        occurrences = list(count.values())
+        return len(occurrences) == len(set(occurrences))
