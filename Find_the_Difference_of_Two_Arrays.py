a6707c05892d1dda7630666f40dd3a111ce0ecfa
Niyathi Mariya George <nmg2881@gmail.com>
2024-06-08 16:45:50 +0530

    Find_the_Difference_of_Two_Arrays.py

--- Find_the_Difference_of_Two_Arrays.py
+++ Find_the_Difference_of_Two_Arrays.py
@@ -20,7 +20,7 @@
 # Every integer in nums2 is present in nums1. Therefore, answer[1] = []
 
 class Solution:
-    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+    def findDifference(self, nums1: list[int], nums2: list[int]) -> list[list[int]]:
         nums1 = set(nums1)
         nums2 = set(nums2)
         return [list(nums1-nums2), list((nums2-nums1))]
