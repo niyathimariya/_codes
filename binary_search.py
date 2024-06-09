9cf1b7cadb8abe54ce8f2487819b2090adc65d74
Niyathi Mariya George <nmg2881@gmail.com>
2024-06-09 12:46:10 +0530

    Added binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,30 +1,33 @@
-def binarySearch(arr, low, high, x):
+class Solution:
+    def binarySearch(self, arr, low, high, x):
+        if high >= low:
+            mid = low + (high - low) // 2
 
-    if high >= low:
+            if arr[mid] == x:
+                return mid
 
-        mid = low + (high - low) // 2
+            elif arr[mid] > x:
+                return self.binarySearch(arr, low, mid-1, x)
 
-        if arr[mid] == x:
-            return mid
-
-        elif arr[mid] > x:
-            return binarySearch(arr, low, mid-1, x)
+            else:
+                return self.binarySearch(arr, mid + 1, high, x)
 
         else:
-            return binarySearch(arr, mid + 1, high, x)
+            return -1
 
-    else:
-        return -1
+solution = Solution()
+arr = [2, 3, 4, 10, 40]
+x = 10
+result = solution.binarySearch(arr, 0, len(arr)-1, x)
+if result != -1:
+    print("Element is present at index", result)
+else:
+    print("Element is not present in array")
 
-if __name__ == '__main__':
-    arr = [2, 3, 4, 10, 40]
-    x = 10
-
-    result = binarySearch(arr, 0, len(arr)-1, x)
     
-    if result != -1:
-        print("Element is present at index", result)
-    else:
-        print("Element is not present in array")
+
+    
+    
+
 
 
