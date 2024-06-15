9b231913a7d48b9197181ff59dd3b755f2e6b1e2
Niyathi Mariya George <nmg2881@gmail.com>
2024-06-15 22:43:34 +0530

    Added bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,22 +1,23 @@
-def bubbleSort(arr):
-    n = len(arr)
+class Solution:
+    def bubbleSort(self, arr):
+        n = len(arr)
 
-    for i in range(n):
-        swapped = False
+        for i in range(n):
+            swapped = False
 
-        for j in range(0, n-i-1):
+            for j in range(0, n-i-1):
+                if arr[j] > arr[j+1]:
+                    arr[j], arr[j+1] = arr[j+1], arr[j]
+                    swapped = True
 
-            if arr[j] > arr[j+1]:
-                arr[j], arr[j+1] = arr[j+1], arr[j]
-                swapped = True
-        if (swapped == False):
-            break
+            if not swapped:
+                break
+# After the inner loop completes, the swapped flag is checked.If no swaps were made (swapped is False), it means the array is already sorted, and the outer loop is exited early to save unnecessary iterations.
+        return arr
 
-if __name__ == "__main__":
-    arr = [64, 34, 25, 12, 22, 11, 90]
-
-    bubbleSort(arr)
-
-    print("Sorted array:")
-    for i in range(len(arr)):
-        print("%d" % arr[i], end=" ")
+solution = Solution()
+arr = [64, 34, 25, 12, 22, 11, 90]
+sorted_arr = solution.bubbleSort(arr)
+print("Sorted array:")
+for i in range(len(sorted_arr)):
+    print("%d" % sorted_arr[i], end=" ")
